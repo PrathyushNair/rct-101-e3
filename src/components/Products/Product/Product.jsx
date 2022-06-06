e9bdd21bda7f72e697a0431bc4c5d1f031427435
PrathyushNair <prathyu360@gmail.com>
2022-06-06 16:47:18 +0530

    changes saved

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,8 +3,15 @@ import { CartContext } from "../../../context/CartContext";
 
 const Product = ({el}) => {
   // Note: this id should come from api
+  let [itmcart,setItemcart]=React.useState({})
+  React.useEffect(()=>{
+    fetch(`http://localhost:8080/cartItems?productId=${el.id}`).then((resp)=>(resp.json())).then((resp)=>{
+      setItemcart({resp})
+    })
+  },[])
   const{addtocart}=React.useContext(CartContext)
   const product = { id: el.id };
+  console.log(itmcart)
   return (
     <div style={{border:"1px solid black"}} data-cy={`product-${product.id}`}>
       <h3 data-cy="product-name">{el.name}</h3>
@@ -12,10 +19,7 @@ const Product = ({el}) => {
       <button onClick={()=>addtocart(product.id)} data-cy="product-add-item-to-cart-button">Add to cart</button>
       <div>
         <button data-cy="product-increment-cart-item-count-button">+</button>
-        <span data-cy="product-count">
-          {
-           0
-          }
+        <span data-cy="product-count">0
         </span>
         <button data-cy="product-decrement-cart-item-count-button">-</button>
         <button data-cy="product-remove-cart-item-button">Remove from cart</button>
