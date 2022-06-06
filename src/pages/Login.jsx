c9053d73fa688eedc1af3360b5980326f5ebd857
PrathyushNair <prathyu360@gmail.com>
2022-06-06 16:53:34 +0530

    added navbar to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar.jsx";
 const Login = () => {
 
   const {auth,password,setPassword,email,setEmail,handlelogin}=useContext(AuthContext)
   console.log(auth)
   return (
     <div>
-    
+      <Navbar/>
       <input onChange={(e)=>setEmail(e.target.value)} data-cy="login-email" />
       <input onChange={(e)=>setPassword(e.target.value)}data-cy="login-password" />
       <button onClick={handlelogin} data-cy="login-submit">Login</button>
